8ff6a7d7fee1c608167f0e2b2b98f644113a90e7
gaohui5@xdf.cn <gaohui5@xdf.cn>
2015-03-11 13:35:18 +0800

    增加登录和注册验证功能

--- client/client.js
+++ client/client.js
@@ -1,57 +1,112 @@
-Session.setDefault("currentUrl",{index:"active",login:"",reg:""});
 
-Session.setDefault("info",{success:"",error:""});
+Session.setDefault("currentUrl", {index: "active", login: "", reg: ""});
 
-Template.info.info =function(){
+Session.setDefault("info", {success: "", error: ""});
+
+Template.info.info = function () {
     return Session.get("info");
 }
 
-Template.container.currentUrl=function(){
+Template.container.currentUrl = function () {
     return Session.get("currentUrl");
 }
-Template.nav.active=function (){
+Template.nav.active = function () {
     return Session.get("currentUrl");
 }
 
 Template.reg.events({
-    'click #submit_button':function(evt) {
-            evt.preventDefault();
-            Accounts.createUser({username: $("#username").val(),password:$("#password").val()},function (err){
-                if(err){
-                    Session.set("info", {success: "", error: err.reason});
-                }else{
-                    Session.set("info",{success:"注册成功" , error:""})
-                }
+    'click #submit_button': function (evt) {
+
+        var $username = $("#username").val();
+        var $password = $("#password").val();
+        var $repeat = $("#password-repeat").val();
+        if ($password.length == 0 || $username.length == 0) {
+            Session.set("info", {success: "", error: "用户名或者密码不能为空"});
+            return;
+        }
+        if ($password != $repeat) {
+            Session.set("info", {success: "", error: "两次输入密码不一致"});
+            return;
+        }
+
+        evt.preventDefault();
+        Accounts.createUser({username: $("#username").val(), password: $("#password").val()}, function (err) {
+            if (err) {
+                Session.set("info", {success: "", error: err.reason});
+            } else {
+                Router.redirect("/");
+                Session.set("info", {success: "注册成功", error: ""})
+            }
         });
     }
 });
 
+/**
+ * event 采用集合方式进行传递事件
+ */
+Template.login.events({
+    'click #login_submit':function(event){
+        event.preventDefault();
+        var $username = $("#username").val();
+        var $password = $("#password").val();
+        if($username.length ==0 || $password.length==0){
+            Session.set("info",{success:"",error:"用户名或密码不能为空"});
+        }
+
+        // 登录使用公共方法登录
+        Meteor.loginWithPassword($username,$password ,function (error){
+            if(error){
+                Session.set("info",{success:"",error:error.reason()});
+            }else{
+                Router.redirect("/");
+                Session.set("info",{success:"登录成功",error:""});
+            }
+        });
+    }
+});
 
 var urlRouter = Backbone.Router.extend({
-    routes:{
-        "" : "index",
-        "login":"login",
-        "reg" : "reg"
+    routes: {
+        "": "index",
+        "login": "login",
+        "reg": "reg",
+        "logout":"logout"
     },
 
-    index:function(){
-        Session.set("currentUrl",{index:"active", login:"",reg:""});
+    index: function () {
+        Session.set("currentUrl", {index: "active", login: "", reg: ""});
     },
 
-    login:function(){
-        Session.set("currentUrl",{index:"",login:"active",reg:""});
+    login: function () {
+        if(Meteor.userId()){
+            this.navigate("/",true);
+            Session.set("info",{success:"用户已经在线",err:""});
+            return ;
+        }
+        Session.set("currentUrl", {index: "", login: "active", reg: ""});
     },
 
 
-    reg:function(){
-        Session.set("currentUrl",{index:"", login:"",reg:"active"});
+    reg: function () {
+        Session.set("currentUrl", {index: "", login: "", reg: "active"});
     },
 
-    redirect:function(url){
+    logout : function (){
+        if(Meteor.userId()){
+            Meteor.logout();
+            Session.set("info",{success:"登出成功",error:""});
+            return ;
+        }else{
+            this.navigate("/",true);
+            Session.set("info",{success:"用户不在线",error:""});
+        }
+    },
+
+    redirect: function (url) {
         this.navigate(url, true);
     }
 });
 Router = new urlRouter;
-Meteor.startup(function (){
+Meteor.startup(function () {
     Backbone.history.start({pushState: true});
 });
